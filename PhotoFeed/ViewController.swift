9f49427057f78e001b7b9da53a1a260f473799b6
Vladislav Gorelov <vsg@yandex.ru>
2023-06-01 21:11:18 +0300

    Initial Commit

---
+++ PhotoFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFeed
+//
+//  Created by Владислав Горелов on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
