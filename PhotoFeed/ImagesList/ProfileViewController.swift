bf5ab3ae519def6b7fb7af926a641aac2701cf5a
Vladislav Gorelov <vsg@yandex.ru>
2023-06-27 13:51:29 +0300

    Сверстан экран профиля, добавлен класс ProfileViewController

---
+++ PhotoFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBAction func exitButton(_ sender: Any) {
+    }
+
+
+}
