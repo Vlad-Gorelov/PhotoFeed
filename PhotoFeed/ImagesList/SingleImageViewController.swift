dcaf46b81e74830c47f3de1d20bc61634049bbd5
Vladislav Gorelov <vsg@yandex.ru>
2023-06-28 20:27:16 +0300

    Добавили экран с отдельной картинкой и настроили переходы

--- PhotoFeed/ImagesList/SingleImageViewController.swift
+++ PhotoFeed/ImagesList/SingleImageViewController.swift
@@ -10,6 +10,10 @@ final class SingleImageViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
 
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
