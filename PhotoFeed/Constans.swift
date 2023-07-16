7a98113e2e68a773d4945d2337320a97137d2e3b
Vladislav Gorelov <vsg@yandex.ru>
2023-07-16 15:25:12 +0300

    Добавили возможность перехода на страницу авторизации Unsplash через вебкит

--- PhotoFeed/Constans.swift
+++ PhotoFeed/Constans.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-private var AccessKey = "gLkc8QG1UQq8IA3AlkD-O6dhMIyE3uU43gaNZ0aw6ME"
-private var SecretKey = "6qbyN1WJcsECpQmC9W-qJ6PYvmi1wf2KIoFopXKmExU"
-private var RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
-private let AccessScope = "public+read_user+write_likes"
-private let DefaultBaseUrl = URL(string: "https://api.unsplash.com")!
+var AccessKey = "gLkc8QG1UQq8IA3AlkD-O6dhMIyE3uU43gaNZ0aw6ME"
+var SecretKey = "6qbyN1WJcsECpQmC9W-qJ6PYvmi1wf2KIoFopXKmExU"
+var RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseUrl = URL(string: "https://api.unsplash.com")!
